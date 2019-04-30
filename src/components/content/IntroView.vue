74cfcb26fefa9b9cefac1d0b6dec88e5876579d1
sidfate <15757176627@163.com>
2019-04-30 17:08:17 +0800

    :hammer: show more info in homepage.

--- src/components/content/IntroView.vue
+++ src/components/content/IntroView.vue
@@ -1,121 +1,82 @@
 <template>
   <div>
     <v-flex xs12>
-      <!--<v-img-->
-              <!--:src="require('../assets/logo.svg')"-->
-              <!--class="my-3"-->
-              <!--contain-->
-              <!--height="200"-->
-      <!--&gt;</v-img>-->
+      <v-img
+              :src="require('@/assets/logo.png')"
+              class="my-3"
+              contain
+              height="200"
+      ></v-img>
     </v-flex>
 
     <v-flex mb-4>
-      <h1 class="display-2 font-weight-bold mb-3">
-        Welcome to ZKCX
-      </h1>
+      <h3 class="display-1 font-weight-bold mb-3">
+        Welcome to ZooKeeperCX
+      </h3>
       <p class="subheading font-weight-regular">
-        A cross-platform GUI Zookeeper Client for developments.
-        <br>please join our online
-        <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+        A Cross-platform | Succinct | Smart GUI ZooKeeper Client Just For You.
       </p>
     </v-flex>
-
-    <v-flex
-            mb-5
-            xs12
-    >
-      <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-      <v-layout justify-center>
-        <a
-                v-for="(next, i) in whatsNext"
-                :key="i"
-                :href="next.href"
-                class="subheading mx-3"
-                target="_blank"
-        >
-          {{ next.text }}
-        </a>
-      </v-layout>
-    </v-flex>
-
     <v-flex
             xs12
             mb-5
     >
-      <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+      <h2 class="headline font-weight-bold mb-3">Links</h2>
 
       <v-layout justify-center>
-        <a
-                v-for="(link, i) in importantLinks"
-                :key="i"
-                :href="link.href"
-                class="subheading mx-3"
-                target="_blank"
+        <v-chip  v-for="item in links"
+                 @click="openExternal(item.link)"
+                 :key="item.name"
+                 style="cursor: pointer"
         >
-          {{ link.text }}
-        </a>
+          <v-avatar>
+            <v-icon>
+              {{ item.icon }}
+            </v-icon>
+          </v-avatar>
+          {{ item.name }}
+        </v-chip>
       </v-layout>
     </v-flex>
   </div>
 </template>
 
 <script>
+  import { shell } from 'electron'
+
   export default {
     name: "IntroView",
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
-
-      ]
-    })
+    data() {
+      return {
+        links: [
+          {
+            name: 'Website',
+            icon: 'mdi-web',
+            link: 'https://github.com/Sidfate/ZooKeeperCX'
+          },
+          {
+            name: 'Github',
+            icon: 'mdi-github-circle',
+            link: 'https://github.com/Sidfate/ZooKeeperCX'
+          },
+          {
+            name: 'Issue',
+            icon: 'mdi-alert-circle-outline',
+            link: 'https://github.com/Sidfate/ZooKeeperCX/issues'
+          },
+          {
+            name: 'Author',
+            icon: 'mdi-account-circle-outline',
+            link: 'https://sidfate.com/'
+          }
+        ]
+      }
+    },
+    methods: {
+      openExternal (link) {
+        shell.openExternal(link)
+      }
+    }
   }
 </script>
 
